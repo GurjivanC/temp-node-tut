f9c43e11f8b443fbfb95b66922cba8e8b37d6460
GurjivanC <97761379+GurjivanC@users.noreply.github.com>
2022-01-30 14:44:20 -0700

    first commit

---
+++ 5-2.js
@@ -0,0 +1,7 @@
+const names = require('./4')
+const sayHi = require('./6')
+const data = require('./6-alt')
+require('./7-mind')
+/* sayHi("susan")
+sayHi(names.john)
+sayHi(names.peter) */
---
+++ 5.js
@@ -0,0 +1,5 @@
+const names = require('./4')
+console.log(names)
+//sayHi("susan")
+//sayHi(john)
+//sayHi(peter)
